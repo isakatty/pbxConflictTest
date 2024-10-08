d5f060f7d8b5a198a2b1b3a9a25964c3bff30b37
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-10-08 11:17:09 +0900

    [Setting] 프로젝트 셋팅

---
+++ pbxConflict/pbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  pbxConflictApp.swift
+//  pbxConflict
+//
+//  Created by Jisoo Ham on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct pbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
